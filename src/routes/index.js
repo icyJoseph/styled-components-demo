f128bbee2feda6dc50e4d436e2e9ae8ec9b35804
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-06 20:16:30 +0200

    feat: use NotFound and card route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Router, Redirect } from "@reach/router";
 
-import Problem from "routes/problem";
+import Cards from "routes/cards";
 import Overrides from "routes/overrides";
 import Theming from "routes/theming";
 
 import NavBar from "components/Navbar";
 
+const NotFound = () => <Redirect noThrow to="/cards" />;
+
 export default () => (
   <Router>
-    <Redirect noThrow from="/" to="problem" />
+    <Redirect noThrow from="/" to="cards" />
     <NavBar path="/">
-      <Problem path="problem" />
+      <Cards path="cards" />
       <Overrides path="overrides" />
       <Theming path="theming" />
+      <NotFound default />
     </NavBar>
   </Router>
 );

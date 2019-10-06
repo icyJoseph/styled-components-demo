3a590a728596638f6d25121271b20853e683c8f5
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-06 20:09:11 +0200

    style: define two themes and create a global style

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    html,body {
+        background-color: ${({ theme }) => theme[theme.name]}
+        color: ${({ theme }) => theme.fontColor}
+    }
+
+`;

7d1253d88020a53a14b31f5f9cd8abc0c70e77ba
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-06 20:10:00 +0200

    feat: toggle light to dark and use global style

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,20 +1,23 @@
 import React, { useState, createContext, useMemo, useContext } from "react";
 import { ThemeProvider } from "styled-components";
 import themeSheet, { LIGHT, DARK } from "styles/theme";
+import GlobalStyle from "styles/globalStyle";
 
 const ThemeChangeContext = createContext({});
+
+const toggler = current => (current === LIGHT ? DARK : LIGHT);
 
 export const ThemeModeProvider = ({ children }) => {
   const [mode, setMode] = useState(LIGHT);
 
-  const setLight = () => setMode(LIGHT);
-  const setDark = () => setMode(DARK);
+  const toggle = () => setMode(toggler);
 
-  const value = useMemo(() => ({ setLight, setDark }), []);
+  const value = useMemo(() => ({ mode, toggle }), [mode]);
   const theme = useMemo(() => ({ ...themeSheet[mode] }), [mode]);
 
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <ThemeChangeContext.Provider value={value}>
         {children}
       </ThemeChangeContext.Provider>

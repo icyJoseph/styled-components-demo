2be5bc7b9fb2cfc6beb72cad90d0ea14de4b6e9b
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-06 20:15:55 +0200

    feat: implement theme switching

--- src/routes/theming.js
+++ src/routes/theming.js
@@ -1,3 +1,26 @@
 import React from "react";
+import { withTheme } from "styled-components";
 
-export default () => <div>Theming</div>;
+import Layout from "components/Layout";
+import { useChangeTheme } from "providers";
+import { LIGHT } from "styles/theme";
+
+import Switch from "components/Switch";
+
+export const Theming = ({ theme }) => {
+  const { mode, toggle } = useChangeTheme();
+  //   console.log(theme);
+  return (
+    <Layout>
+      <Switch
+        id="theme"
+        name="theme-switch"
+        label={`${mode} - 💅`}
+        isOn={mode === LIGHT}
+        handleToggle={toggle}
+      />
+    </Layout>
+  );
+};
+
+export default withTheme(Theming);

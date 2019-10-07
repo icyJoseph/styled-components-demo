fe6d9f824b13059d66bc5a54c26a1efc7b1899b5
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-10-07 09:52:48 +0200

    style: use relative units for Switch

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -15,10 +15,10 @@ const SwitchWrap = styled.div`
     align-items: center;
     justify-content: space-between;
     cursor: pointer;
-    width: 100px;
-    height: 50px;
+    width: 6.66em;
+    height: 3.33em;
     background: ${({ isOn }) => (isOn ? "#06D6A0" : "grey")};
-    border-radius: 100px;
+    border-radius: 6.66em;
     position: relative;
     transition: background-color 0.2s;
   }
@@ -28,9 +28,9 @@ const SwitchWrap = styled.div`
     position: absolute;
     top: 2px;
     left: 2px;
-    width: 45px;
-    height: 45px;
-    border-radius: 45px;
+    width: 3em;
+    height: 3em;
+    border-radius: 3em;
     transition: 0.2s;
     background: #fff;
     box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
